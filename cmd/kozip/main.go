b95badb34863d9faf1daa6a880b52edf2da4e534
Kyungwook Park <parksama@gmail.com>
2026-03-28 09:19:22 +0900

    feat: add structured exit codes to entrypoint

--- cmd/kozip/main.go
+++ cmd/kozip/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/kyungw00k/kozip/api"
 	"github.com/kyungw00k/kozip/internal/cli"
 )
 
 func main() {
 	if err := cli.Execute(); err != nil {
+		if apiErr, ok := api.AsAPIError(err); ok {
+			fmt.Fprintln(os.Stderr, apiErr.Error())
+			os.Exit(apiErr.ExitCode())
+		}
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
